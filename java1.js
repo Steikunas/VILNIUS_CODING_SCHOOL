050862b44d61029b4414fd19e690bd3261603138
Tomas Steikunas <tsteikunas@gmail.com>
2021-11-30 21:33:31 +0200

    bandymas1 ikelta uzduotis

--- java1.js
+++ java1.js
@@ -7,3 +7,19 @@ a=2;
 tekstas= 'kintamojo a reikšmė: ';
 
 console.log(tekstas, a);
+
+let b= 'Mano vardas';
+let c= 'Tomas Steikunas';
+
+console.log(b, c);
+
+let metai=1986;
+let menuo=0o6;
+let diena=15;
+
+console.log(metai%10+diena%10+menuo%10);
+
+r=15;
+pi=3.14;
+console.log(2*pi*r);
+console.log(r*r*pi);
