510ef93cbb3be975e91aa8622b0e5b9d1be2bda8
Tomas Steikunas <tsteikunas@gmail.com>
2021-12-08 21:41:54 +0200

    Java mokymai

--- java1.js
+++ java1.js
@@ -1,47 +1,7 @@
-let e=1;
-let tekstas= 'kintamojo e reikšmė: ';
 
-console.log(tekstas, e);
+$(document).ready(function(){
+    $('*').click(function(){
+        $("#kasas").text('TagName');
 
-e=2;
-tekstas= 'kintamojo e reikšmė: ';
-
-console.log(tekstas, e);
-
-let e= 'Mano vardas';
-let f= 'Tomas Steikunas';
-
-console.log(b, c);
-
-let metai=1986;
-let menuo=0o6;
-let diena=15;
-
-console.log(metai%10+diena%10+menuo%10);
-
-r=15;
-pi=3.14;
-console.log(2*pi*r);
-console.log(r*r*pi);
-
-let a=3;
-let b=7;
-let c=5;
-let d=9;
-let answer;
-
-if ((c<b) && (c>a) && (d>b)) {answer = 'Rezis' + [c,b]}
-else if ((c<b) && (c<=a) && (d>b)) {answer ='Rezis' +  c,b}
-else if ((c<b) && (c>a) && (d<b)) {answer = 'Rezis' +  c , d}
-else if ((c<b) && (c<a) && (d<b)) {answer = 'Rezis' +  c , d}
-else if (c>b) {answer = 'Null'};
-console.log(answer);
-
-
-let x='Vardas';
-let y='Pavardė';
-let z='Kur mokosi';
-let w= '5,7,8,6,5,3';
-let atsakymas = x + y + z + w;
-document.getElementById("demo").innerHTML = 'atsakymas';
-
+});
+});
